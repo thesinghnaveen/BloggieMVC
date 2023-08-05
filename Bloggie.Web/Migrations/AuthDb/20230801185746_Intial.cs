557b74a9c6df741804bf04a53bb03e9c3694dbf5
Naveen Singh <naveen1999peppeganj@gmail.com>
2023-08-05 10:20:47 +0530

    minor changes

---
+++ Bloggie.Web/Migrations/AuthDb/20230801185746_Intial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class Intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "472ba632-6133-44a1-b158-6c10bd7d850d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "69875565-188f-483b-a978-40ae3d7b35aa", "AQAAAAIAAYagAAAAEA1Vzhntz7Q8DHN/0GMGrROZZtYx8iMLjskxGb3A2g3FSEXdZp19hIlX9tZMYWa0DA==", "8913edd2-9c44-4ae3-a23a-c878cb423fb5" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "472ba632-6133-44a1-b158-6c10bd7d850d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9b34b502-4d24-418c-9685-6ecc5b92f84a", "AQAAAAIAAYagAAAAED0HnnZg5BTzC6vBfWoRQNScX55RM0YMARMiwWfa81uBIz2aND0ptZSVN7KM8lJr/g==", "c2714ebb-1ea6-4679-ae35-3379f866210e" });
+        }
+    }
+}
--- Bloggie.Web/Migrations/AuthDb/AuthDbContextModelSnapshot.cs
+++ Bloggie.Web/Migrations/AuthDb/AuthDbContextModelSnapshot.cs
@@ -166,15 +166,15 @@ namespace Bloggie.Web.Migrations.AuthDb
                         {
                             Id = "472ba632-6133-44a1-b158-6c10bd7d850d",
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "9b34b502-4d24-418c-9685-6ecc5b92f84a",
+                            ConcurrencyStamp = "69875565-188f-483b-a978-40ae3d7b35aa",
                             Email = "superadmin@bloggie.com",
                             EmailConfirmed = false,
                             LockoutEnabled = false,
                             NormalizedEmail = "SUPERADMIN@BLOGGIE.COM",
                             NormalizedUserName = "SUPERADMIN@BLOGGIE.COM",
-                            PasswordHash = "AQAAAAIAAYagAAAAED0HnnZg5BTzC6vBfWoRQNScX55RM0YMARMiwWfa81uBIz2aND0ptZSVN7KM8lJr/g==",
+                            PasswordHash = "AQAAAAIAAYagAAAAEA1Vzhntz7Q8DHN/0GMGrROZZtYx8iMLjskxGb3A2g3FSEXdZp19hIlX9tZMYWa0DA==",
                             PhoneNumberConfirmed = false,
-                            SecurityStamp = "c2714ebb-1ea6-4679-ae35-3379f866210e",
+                            SecurityStamp = "8913edd2-9c44-4ae3-a23a-c878cb423fb5",
                             TwoFactorEnabled = false,
                             UserName = "superadmin@bloggie.com"
                         });
